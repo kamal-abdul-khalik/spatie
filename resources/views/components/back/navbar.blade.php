e2f377055b5e5bed1ebdf0a4e20a19c5f7b2ccf0
gururpl <elsakcf@gmail.com>
2020-10-26 03:33:50 +0800

    add logout

--- resources/views/components/back/navbar.blade.php
+++ resources/views/components/back/navbar.blade.php
@@ -15,16 +15,14 @@
                 <a href="features-profile.html" class="dropdown-item has-icon">
                     <i class="far fa-user"></i> Profile
                 </a>
-                <a href="features-activities.html" class="dropdown-item has-icon">
-                    <i class="fas fa-bolt"></i> Activities
-                </a>
-                <a href="features-settings.html" class="dropdown-item has-icon">
-                    <i class="fas fa-cog"></i> Settings
-                </a>
                 <div class="dropdown-divider"></div>
-                <a href="#" class="dropdown-item has-icon text-danger">
+                <a href="/logout" onclick="event.preventDefault();
+                document.getElementById('logout-form').submit();" class="dropdown-item has-icon text-danger">
                     <i class="fas fa-sign-out-alt"></i> Logout
                 </a>
+                <form id="logout-form" action="/logout" method="POST" style="display: none;">
+                    @csrf
+                </form>
             </div>
         </li>
     </ul>
