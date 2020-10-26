6ce8a6adc3e8709ca851fd8769f56774d6d8305e
gururpl <elsakcf@gmail.com>
2020-10-27 04:01:55 +0800

    refactor user controller

---
+++ app/Http/Controllers/Permissions/UserController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Controllers\Permissions;
+
+use App\Http\Controllers\Controller;
+use App\Models\{User, Role};
+
+class UserController extends Controller
+{
+    public function create()
+    {
+        return view('back.permissions.assign.user.create', [
+            'roles' => Role::get(),
+            'users' => User::has('roles')->get(),
+        ]);
+    }
+
+    public function store()
+    {
+        request()->validate([
+            'email' => 'required|email',
+            'roles' => 'array|required'
+        ]);
+
+        $user = User::where('email', request('email'))->first();
+        if ($user == null) {
+            return redirect()->back()
+                ->with('error', request('email') . ' tidak ditemukan dalam database');
+        }
+
+        $user->assignRole(request('roles'));
+        return redirect()->back()
+            ->with('success', "{$user->email} has been assign to the role ");
+    }
+
+    public function edit(User $user)
+    {
+        return view('back.permissions.assign.user.edit', [
+            'user' => $user,
+            'roles' => Role::get(),
+            'users' => User::has('roles')->get(),
+        ]);
+    }
+
+    public function update(User $user)
+    {
+        $user->syncRoles(request('roles'));
+
+        return redirect()->route('assign.user.create')
+            ->with('success', "{$user->email} has been sync to the role ");
+    }
+}
