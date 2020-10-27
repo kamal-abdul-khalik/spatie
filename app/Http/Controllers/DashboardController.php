5dfc85556511a0734d2bb82de0c2c83c0b6952cf
gururpl <elsakcf@gmail.com>
2020-10-27 22:37:53 +0800

    use App\Models\Role

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Role;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
@@ -14,6 +15,8 @@ class DashboardController extends Controller
      */
     public function __invoke(Request $request)
     {
-        return view('dashboard');
+        return view('dashboard', [
+            'roles' => Role::get(),
+        ]);
     }
 }
